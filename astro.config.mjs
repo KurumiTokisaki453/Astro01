e0feada28ae08cf7338b5879e7891e56d13e99d9
kurum <xxforestal453@gmail.com>
2023-10-13 10:23:45 -0500

    Github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kurumitokisaki453.github.io',
+  base: '/astro01/',
+});
